191a8aa5a5b2f4d3f0593058a001ea869b0abf4b
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-12 15:45:48 +0600

    Added tracks.ts

---
+++ api/routers/tracks.ts
@@ -0,0 +1,36 @@
+import {Router} from "express";
+import Track from "../models/Track";
+
+const tracksRouter = Router();
+
+tracksRouter.get('/', async(req,res, next) => {
+    try {
+        const album = req.query.album;
+        if(album) {
+            const tracks = await Track.find({album: req.query.album});
+            return res.send(tracks);
+        } else {
+            const tracks = await Track.find();
+            return res.send(tracks);
+        }
+
+    } catch(e) {
+        return next(e);
+    }
+});
+
+tracksRouter.post('/', async(req, res, next) => {
+   try {
+       const trackData = {
+           name: req.body.name,
+           album: req.body.album,
+           length: req.body.length,
+       }
+       const track = new Track(trackData);
+       await track.save();
+       res.send(track);
+
+   } catch(e) {
+       next(e);
+   }
+});
