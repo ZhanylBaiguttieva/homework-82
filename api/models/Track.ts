7cd595e6c8be1d8f75038896ec66a3cccc0924cb
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-12 14:53:13 +0600

    Added models

---
+++ api/models/Track.ts
@@ -0,0 +1,26 @@
+import mongoose, {Schema, Types} from "mongoose";
+import Album from "./Album";
+
+const TrackSchema = new mongoose.Schema({
+    album: {
+        type: Schema.Types.ObjectId,
+        ref:'Album',
+        required: true,
+        validate: {
+            validator: async (value: Types.ObjectId) => {
+                 const album = await Album.findById(value);
+                 return Boolean(album);
+            },
+            message: 'Album does not exist',
+        }
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    length: String,
+});
+
+const Track = mongoose.model('Track', TrackSchema);
+
+export default Track;
