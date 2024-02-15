1c54f09cf238f072cd2b3fe8dd59fcb61bf742db
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-15 14:22:42 +0600

    added usersRouter

--- api/index.ts
+++ api/index.ts
@@ -6,6 +6,7 @@ import config from './config';
 import artistsRouter from "./routers/artists";
 import albumsRouter from "./routers/albums";
 import tracksRouter from "./routers/tracks";
+import usersRouter from "./routers/users";
 
 const app = express();
 const port = 8000;
@@ -17,6 +18,7 @@ app.use(cors());
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
 app.use('/tracks', tracksRouter);
+app.use('/users', usersRouter);
 
 const run = async () => {
     await mongoose.connect(config.mongoose.db);
