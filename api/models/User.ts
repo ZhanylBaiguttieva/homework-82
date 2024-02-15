a225fa25bb41c707fe1d095a59df3d241040169d
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-15 14:05:15 +0600

    Added model User

---
+++ api/models/User.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    token: {
+        type: String,
+        required: true,
+    }
+});
+
+UserSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.set('toJSON',{
+    transform: (doc, ret, options) => {
+        delete ret.password;
+        return ret;
+    }
+});
+
+const User = mongoose.model ('User', UserSchema);
+export default User;
