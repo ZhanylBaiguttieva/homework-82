866499251706384f40faf7f9b8a7b1d0eae5ebeb
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-22 16:02:35 +0600

    Added Route for albums

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Toolbar from './UI/ToolBar.tsx';
 import { Container, CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 import Artists from './features/artists/components/Artists.tsx';
+import Albums from './features/albums/components/Albums.tsx';
 
 function App() {
 
@@ -17,6 +18,8 @@ function App() {
         <Container maxWidth="xl">
           <Routes>
             <Route path="/" element={<Artists />} />
+            <Route path="/albums/:id" element={<Albums />} />
+            {/*<Route path="/albums" element={<Albums/>}/>*/}
             <Route path="*" element={<h1>Not found</h1>} />
           </Routes>
         </Container>
