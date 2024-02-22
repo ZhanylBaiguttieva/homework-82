1a8827e050f7b0c4522dcb26f0480f551d14f490
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-22 14:33:04 +0600

    Added Route for Album

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
 
 import './App.css';
+import Toolbar from './UI/ToolBar.tsx';
+import { Container, CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Artists from './features/artists/components/Artists.tsx';
 
 function App() {
 
-
   return (
     <>
-
+      <CssBaseline/>
+      <header>
+        <Toolbar/>
+      </header>
+      <main>
+        <Container maxWidth="xl">
+          <Routes>
+            <Route path="/" element={<Artists />} />
+            <Route path="*" element={<h1>Not found</h1>} />
+          </Routes>
+        </Container>
+      </main>
     </>
   );
 }
