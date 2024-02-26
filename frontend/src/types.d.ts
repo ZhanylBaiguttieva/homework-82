b81ab920e013f7d36c714c3c6defa1fe8e19b92a
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-26 14:30:35 +0600

    HW#86 - Added Register.tsx and usersSlice.ts and usersThunk.ts

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -19,3 +19,32 @@ export interface Track {
   number: number;
   length: string;
 }
+
+export interface User {
+  _id: string;
+  username: string;
+  token: string;
+}
+
+export interface RegisterMutation {
+  username: string;
+  password: string;
+}
+
+export interface ValidationError {
+  errors: {
+    [key: string]: {
+      name: string;
+      message: string;
+    }
+  },
+  message: string;
+  name: string;
+  _message: string;
+}
+export interface RegisterResponse {
+  message: string;
+  user: User;
+}
+
+
