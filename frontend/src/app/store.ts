4778dbf7095619ede01f84699848f90141d59012
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-26 15:23:12 +0600

    HW#86 - Added  redux-persist

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,17 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { artistsReducer } from '../features/artists/artistsSlice.ts';
 import { albumsReducer } from '../features/albums/albumsSlice.ts';
 import { tracksReducer } from '../features/tracks/tracksSlice.ts';
 import { usersReducer } from '../features/users/usersSlice.ts';
+import {persistReducer, FLUSH, PAUSE, PERSIST, REHYDRATE, PURGE, REGISTER, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 
-export const store = configureStore({
-    reducer: {
-      artists: artistsReducer,
-      albums: albumsReducer,
-      tracks: tracksReducer,
-      users: usersReducer,
-    }
+const usersPersistConfig = {
+  key:'shop:users',
+  storage: storage,
+  whiteList: ['user'],
+};
+
+const rootReducer = combineReducers({
+  artists: artistsReducer,
+  albums: albumsReducer,
+  tracks: tracksReducer,
+  users: persistReducer(usersPersistConfig, usersReducer),
 });
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, PAUSE, PERSIST, REHYDRATE, PURGE, REGISTER],
+    },
+  }),
+});
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,15 @@ import App from './App';
 import './index.css';
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
-import { store } from './app/store.ts';
-
+import { persistor, store } from './app/store.ts';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
         <BrowserRouter>
-                <App/>
+          <App/>
         </BrowserRouter>
+      </PersistGate>
     </Provider>
 );
